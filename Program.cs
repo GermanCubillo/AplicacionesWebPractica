466259369d9741c9aaf50fd805366a3dbde83724
GermanCubillo <31835856+GermanCubillo@users.noreply.github.com>
2017-09-10 21:15:14 -0600

    Ejercicio
    Practica

---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            numeros numero = new numeros();
+            numero.Numero = 11;
+            if (numero.Numero %2 == 0)
+            {
+                Console.WriteLine(numero.NumeroPar());
+            }
+            else
+            {
+                Console.WriteLine(numero.NumeroImpar());
+            }
+            Console.ReadKey();
+        }
+    }
+}
---
+++ numeros.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication2
+{
+    class numeros
+    {
+        public int Numero { get; set; }
+
+        public string NumeroPar ()
+        {
+            return "Numero Par: " + Numero;
+        }
+        public string NumeroImpar()
+        {
+            return "Numero Impar: " + Numero;
+        }
+    }
+}
